6765dcdb57bddad6505eac7670840b67a65a3c19
Simon Vergauwen <nomisRev@users.noreply.github.com>
2022-05-01 11:03:36 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+  kotlin("jvm") version "1.6.21"
+  id("io.kotest.multiplatform") version "5.2.3"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation(kotlin("stdlib"))
+  implementation("io.arrow-kt:arrow-core:1.1.2")
+  implementation("io.arrow-kt:arrow-optics:1.1.2")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+  implementation("io.arrow-kt:arrow-fx-coroutines:1.1.2")
+
+  testImplementation("io.kotest:kotest-property:5.2.3")
+  testImplementation("io.kotest:kotest-assertions-core:5.2.3")
+  testImplementation("io.kotest.extensions:kotest-assertions-arrow:1.2.5")
+  testImplementation("io.kotest.extensions:kotest-property-arrow:1.2.5") // optional
+  testImplementation("io.kotest.extensions:kotest-property-arrow-optics:1.2.5") // optional
+  testImplementation("io.kotest:kotest-runner-junit5-jvm:5.2.3")
+}
